45e79fe6a339087278239b55b6e7a0b43c904c4b
franco18fernandez-hash <franco18fernandez@gmail.com>
2026-02-22 19:26:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -25,3 +25,16 @@ app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
+function abrir(url) {
+    // Si estás en Android, este protocolo intenta abrir Chrome directamente
+    const urlChrome = "intent://" + url.replace("https://", "") + "#Intent;scheme=https;package=com.android.chrome;end";
+    
+    // Intentamos abrir Chrome
+    window.location.href = urlChrome;
+
+    // Si en 500ms no pasó nada (porque no es Android o falló), lo abre normal
+    setTimeout(() => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    }, 500);
+}
+
